059d30713236cc90ee4728d8d3fb82cefe026511
Yash Agarwal <yashagarwaljpr@gmail.com>
2017-02-21 15:37:49 +0530

    fixed minor bugs

--- content/page/quotes.md
+++
@@ -1,9 +0,0 @@
-+++
-title = "Quotes"
-Categories = []
-Tags = []
-Description = ""
-date = "2017-02-21T14:17:03+05:30"
-type = "page"
-+++
-
---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to Github...\033[0m"
+
+# Build the project.
+hugo
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+echo "Please enter commit message: "
+read msg
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+cd public/
+git add -A
+git commit -m "$msg"
+git push origin master
